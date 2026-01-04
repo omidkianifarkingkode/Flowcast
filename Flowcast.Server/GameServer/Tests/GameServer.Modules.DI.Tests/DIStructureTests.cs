993b3234c98400e04aa6748f522c010959847997
Farzin Ebrahimi <Farzinebrahimi.info@gmail.com>
2026-01-04 13:44:56 +0330

    feat:
    -Init test for DI.

---
+++ Flowcast.Server/GameServer/Tests/GameServer.Modules.DI.Tests/DIStructureTests.cs
@@ -0,0 +1,16 @@
+﻿namespace GameServer.Modules.DI.Tests;
+
+public class DIStructureTests
+{
+    private const string ApplicationNameSpace = "Application";
+    private const string ContractsNameSpace = "Contracts";
+    private const string DomainNameSpace = "Domain";
+    private const string InfrastructureNameSpace = "Infrastructure";
+    private const string PresentationNameSpace = "Presentation";
+    
+    [Fact]
+    public void Domain_Should_Not_HaveDependency_On_Application()
+    {
+        
+    }
+}
